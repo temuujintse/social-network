239421773114960f6b36f5ab9608717f1110c488
temuujintse <temuujintserenjav@gmail.com>
2025-04-24 14:41:14 +0800

    result,

--- scripts/analyze_network.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-
-url = "https://raw.githubusercontent.com/datasets/sna-datasets/main/edges.csv"
-data = pd.read_csv(url)
-data.to_csv("data/edges.csv", index=False)
--- scripts/lab12/lab12.py
+++ scripts/lab12/lab12.py
@@ -46,5 +46,48 @@ else:
 # Degree at time 3
 print("Degree of Eddard-Stark at t=3:", g.degree(t=3).get('Eddard-Stark', 0))
 
-# Coverage
+# Coverageimport dynetx as dn
+import networkx as nx
+import random
+
+def read_net(filename):
+    g = nx.Graph()
+    with open(filename) as f:
+        f.readline()
+        for line in f:
+            i = line.split(",")
+            g.add_edge(line[0], line[1])
+    return g
+
+g = dn.DynGraph()
+
+for t in range(1,9):
+  er = read_net(f'/content/asoiaf-book1-edges.csv')
+  g.add_interactions_from(er.edges, t=t)
+
+g.temporal_snapshots_ids()
+
+g1 = g.time_slice(1)
+g1.number_of_nodes()
+g1.number_of_edges()
+
+g1_flat = nx.Graph(g1.edges())
+
+r = g.inter_event_time_distribution()
+print(f"Number interactions: temporal distance\t{r}")
+
+r = g.inter_event_time_distribution("M")
+print(f"Number interactions: temporal distance\t{r}")
+
+print(list(g.edges()))
+
+u = 'A'
+v = 'd'
+
+r = g.inter_event_time_distribution(u, v)
+print(f"Number interactions: temporal distance\t{r}")
+
+g.degree(t=3)['A'] # degree of node 0 at time t=2
+
+g.coverage()
 print("Temporal coverage:", g.coverage())
